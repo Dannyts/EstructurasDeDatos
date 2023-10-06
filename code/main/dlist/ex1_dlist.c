b9e5ceff64995e74fdb9c866f1cf8a82c05b799a
Sp4rk117 <ernesto10sec@gmail.com>
2023-10-05 21:26:06 -0600

    primer codigo

---
+++ code/main/dlist/ex1_dlist.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dlist.h"
+
+/*
+	Print list
+*/
+static void print_dlist (constList *DList) {
+	DListNode *node;
+	int *data, i;
+	//Imprime el tamaño de la lista
+	fprintf(stdout, "List size is %d\n", dlist_size(list));
+
+	i = 0;
+	node=dlist_head(list);
+
+	while (1) {
+		data = dlist_data(node);
+		// imprime el nodo, data, el puntero del nodo yy el puntero al siguiente nodo, asi como el anterior
+		fprintf(stdout, "Dlist_node[%03d]=%03d, %p -> %p -> %p \n", i,*data, node->prev, node, node->next);
+		
+		i++;
+		if (dlist_is_tail (node))
+			break;
+		else
+			node =dlist_next(node);
+	}
+
+	return;
+}
+/*
+	Programa principal
+*/
+int main (int argc, char **argv){
+	DList dlist;
+	DListNode *node;
+
+	int *dat, i;
+
+	//Incializando la lista doble mente ligada
+	dlist_init (&dlist, free);
+
+	//LLenando la lista doblemente ligada
+	node = dlist_head(&dlist);
+	for (i = 30; i > 20; i--) {
+
+		if ((data=(int *)malloc(sizeof(int))) == NULL)
+			return 1;
+
+		*data = i;
+
+		if (dlist_ins_next(&dlist, NULL, data) != 0)
+			return 1;
+	}
+
+	print_dlist(&dlist);
+
+	//iniciando a navegar por la lista doblemente ligada
+	node= dlist_head(&dlist);
+
+	for 
+}
